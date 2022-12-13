38f4b93a5b80f6b18c709110e017f83e9cf1ac40
shashank lalit patil <shashankpatil125@gmail.com>
2022-12-13 07:22:38 +0000

    nru

---
+++ shellScript/reverseString.sh
@@ -0,0 +1,11 @@
+str="$1"
+reversed_string=""
+# finding the length of string
+len=${#str}
+# traverse the string in reverse order.
+for (( i=$len-1; i>=0; i-- ))
+do
+   reversed_string="$reversed_string${str:$i:1}"
+done
+# printing the reversed string.
+echo "$reversed_string"
